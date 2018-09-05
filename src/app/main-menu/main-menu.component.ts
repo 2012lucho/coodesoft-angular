0a77ac3e895fe0e57f17a711c19f1ba73ccea558
LuchoV <lucho.2012.tandil@gmail.com>
2018-09-05 02:17:34 -0300

    [Menu principal] Ahora al hacer scroll pasada la primer página, se aplica fixed top

--- src/app/main-menu/main-menu.component.ts
+++ src/app/main-menu/main-menu.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-main-menu',
@@ -6,10 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main-menu.component.css']
 })
 export class MainMenuComponent implements OnInit {
+  @HostListener("window:scroll", ['$event'])
+  OnWindowsScroll($event:Event){
+    this.pageY = ($event as any).pageY;
+    if (this.pageY > this.screenHeight){
+      this.fixedTop = true;
+    } else {
+      this.fixedTop = false;
+    }
+  }
+
+  @HostListener('window:resize', ['$event'])
+    onResize(event?) {
+      this.screenHeight = window.innerHeight;
+  }
+
+  screenHeight;
+  pageY;
+
+  fixedTop = false;
 
   constructor() { }
 
   ngOnInit() {
+    this.onResize();
   }
 
 }
